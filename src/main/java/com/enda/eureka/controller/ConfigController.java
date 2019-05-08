e7a6d01bc3cfaf26361f581f518cad31440c894d
wt <1139914806@qq.com>
2019-05-08 18:57:25 +0800

    提取配置文件到配置中心

---
+++ src/main/java/com/enda/eureka/controller/ConfigController.java
@@ -0,0 +1,17 @@
+package com.enda.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+//    @Value("${server.port}")
+//    private String name;
+//
+//    @GetMapping("getConfig")
+//    public String get() {
+//        return name;
+//    }
+}
